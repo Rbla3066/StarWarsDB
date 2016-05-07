2373011836876f32060d794def323ded5b003f41
Robert Black <rbla4655@gmail.com>
2016-05-07 11:00:25 -0400

    heroku test'

--- app/config/connection.js
+++ app/config/connection.js
@@ -5,10 +5,10 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
     port: 3306,
-    host: 'localhost',
-    user: 'root',
-    password: "Njmitx123",
-    database: "starwars"
+    host: 'l9dwvv6j64hlhpul.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user: 'qqsiq5izfxbg6soe',
+    password: "beg46amegsim0lyo",
+    database: "wxfh0y4hosqvkzau"
 });
 
 
